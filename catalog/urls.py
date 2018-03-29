6d47a2c7f175316208a1263343494ad016fbe91c
ARS <arsakavov@gmail.com>
2018-03-29 22:39:33 +0300

    add forms urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,3 +8,11 @@ urlpatterns = [
     url(r'^books/$',views.BookListView.as_view(),name='books'),
     url(r'^book/(?P<pk>\d+)$',views.BookDetailView.as_view(), name='book-detail'),
 ]
+
+urlpatterns+=[
+    url(r'^mybooks/$',views.LoanedBooksByUserListView.as_view(),name='my-borrowed'),
+]
+
+urlpatterns += [
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
